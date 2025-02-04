f3fe0cd20f6ecce0440f2c14be872930471629a7
Parbin29 <naazu1295@gmail.com>
2025-02-04 22:40:52 +0100

    feat: added the front end for the todo app

--- client/todo.js
+++ client/todo.js
@@ -1,7 +1,7 @@
 const callToDoAPI = async () => {
     document.getElementById("todo-list").innerHTML = "";
     // let searchTerm = document.getElementById("todo-name");
-    let apiUrl = `http://localhost:5293/api/tasks`;
+    let apiUrl = `http://localhost:5293/api/Task`;
 
     let options = {
         method: 'GET', // or POST, PUT, DELETE, etc.
@@ -14,6 +14,24 @@ const callToDoAPI = async () => {
     const response = await fetch(apiUrl, options);
     const data = await response.then(res => res.json());
        console.log(data);
+}
 
-   
+function addTask() {
+    let taskInput = document.getElementById("taskInput");
+    let taskText = taskInput.value.trim();
+    if (taskText === "") return;
+    
+    let li = document.createElement("li");
+    li.innerHTML = `<span onclick="toggleTask(this)">${taskText}</span> <button onclick="removeTask(this)">X</button>`;
+    document.getElementById("taskList").appendChild(li);
+    
+    taskInput.value = "";
+}
+
+function removeTask(button) {
+    button.parentElement.remove();
+}
+
+function toggleTask(span) {
+    span.classList.toggle("completed");
 }
